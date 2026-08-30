9eba98bf6420ae27c56bf2f009728b5bbd765140
Žan Grčar <grcar.zan1@gmail.com>
2026-08-30 20:56:53 +0200

    Replace PWA with native Android project

--- src/utils/googleMaps.ts
+++
@@ -1,8 +0,0 @@
-export function buildGoogleMapsDirectionsUrl(lat: number, lon: number): string {
-  const params = new URLSearchParams({
-    api: "1",
-    destination: `${lat},${lon}`,
-    travelmode: "driving",
-  });
-  return `https://www.google.com/maps/dir/?${params.toString()}`;
-}
